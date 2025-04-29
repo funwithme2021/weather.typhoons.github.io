ad83b17c4d5a7cd5bfb3b5fce14264b231a1dc50
funwithme2021 <125599198+funwithme2021@users.noreply.github.com>
2025-04-30 05:58:52 +0800

    Add files via upload

---
+++ 颱風氣象預報中心/contact.js
@@ -0,0 +1,28 @@
+// contact.js
+const messages = [
+    {
+        time: "2025-04-28T16:43:00+08:00",account: "zky_06bo",
+        pmId: "1069852656",status: "已回覆",note: "優先訊息"
+      },
+    {
+        time: "2025-04-29T22:27:00+08:00",account: "cc__411",
+        pmId: "1519174032",status: "已回覆",note: "優先訊息"
+      },
+    {
+      time: "2025-04-28T20:14:00+08:00",account: "anika_0127",
+      pmId: "410595602",status: "已回覆",note: "優先訊息"
+    },
+    {
+        time: "2025-04-28T17:32:00+08:00",account: "_a.fu",
+        pmId: "812408700",status: "已回覆",note: "優先訊息"
+      },
+      {
+        time: "2025-04-28T07:53:00+08:00",account: "zhengyi_03_09",
+        pmId: "1720679552",status: "已回覆",note: "優先訊息"
+      },
+      {
+        time: "2025-04-28T04:13:00+08:00",account: "evelyn._.wu.chu__",
+        pmId: "1176570837",status: "已回覆",note: "優先訊息"
+      },
+  ];
+  
