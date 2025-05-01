a3b3f3ebe149fdb592ba36192df5ec2e3a26b6dc
funwithme2021 <125599198+funwithme2021@users.noreply.github.com>
2025-05-01 12:48:10 +0800

    Add files via upload

--- 颱風氣象預報中心/contact.js
+++ 颱風氣象預報中心/contact.js
@@ -1,6 +1,10 @@
 // contact.js
 const messages = [
     {
+        time: "2025-04-28T10:33:00+08:00",account: "sherry__0604",
+        pmId: "914266946",status: "未回覆",note: "普通訊息"
+      },
+    {
         time: "2025-04-28T16:43:00+08:00",account: "zky_06bo",
         pmId: "1069852656",status: "已回覆",note: "優先訊息"
       },
@@ -23,6 +27,38 @@ const messages = [
       {
         time: "2025-04-28T04:13:00+08:00",account: "evelyn._.wu.chu__",
         pmId: "1176570837",status: "已回覆",note: "優先訊息"
+      },
+      {
+        time: "2025-04-30T04:18:45+08:00",account: "littlechinright",
+        pmId: "1059489958",status: "已回覆",note: "普通訊息"
+      },
+      {
+        time: "2025-04-29T04:23:13+08:00",account: "lulu_920120",
+        pmId: "2113579241",status: "已回覆",note: "普通訊息"
+      },
+      {
+        time: "2025-04-25T04:23:13+08:00",account: "TEST",
+        pmId: "21105975",status: "未回覆",note: "普通訊息"
+      },
+      {
+        time: "2025-04-25T04:23:13+08:00",account: "TEST",
+        pmId: "21105975",status: "未回覆",note: "普通訊息"
+      },
+      {
+        time: "2025-04-25T04:23:13+08:00",account: "TEST",
+        pmId: "21105975",status: "未回覆",note: "普通訊息"
+      },
+      {
+        time: "2025-04-25T04:23:13+08:00",account: "TEST",
+        pmId: "21105975",status: "未回覆",note: "普通訊息"
+      },
+      {
+        time: "2025-04-25T04:23:13+08:00",account: "TEST",
+        pmId: "21105975",status: "未回覆",note: "普通訊息"
+      },
+      {
+        time: "2025-04-25T04:23:13+08:00",account: "TEST",
+        pmId: "21105975",status: "未回覆",note: "普通訊息"
       },
   ];
   
